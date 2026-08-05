bca378ec208d3ed050b86025540f87dee74cde6e
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-08-05 12:37:11 +0530

    using set -e to check automatic exit if error comes kept set and tram in common.sh and using it in mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -10,26 +10,15 @@ echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf install mysql-servdder -y &>>$LOGFILE
-#VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
-#VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
-#VALIDATE $? "Starting MySQL Server"
-
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIDATE $? "Setting up root password"
-
-# Below code is will be useful for idempotent nature
-# -e is to enable for command --> show databases
-# First time while running if block executes and sets up password
-# Second time when ran the password is already setup it will skip setting password agian
 
 mysql -h db.daws-78s.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} $>>$LOGFILE
+    mysql_secure_indcsdvstallation --set-root-pass ${mysql_root_password} $>>$LOGFILE
     #VALIDATE $? "MySQL root password setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
