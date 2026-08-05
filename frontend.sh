b2e546e2c7ddc790a081e5aa81ec7ace2b61cb86
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-08-05 14:25:29 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -26,7 +26,7 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 # check your repo and path
-cp /home/ec2-user/expense-shell-re/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/expense-shell-1-re/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
