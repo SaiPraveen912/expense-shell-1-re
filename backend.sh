25094fb12fea40fa934d9471a8bf9aa64beefa3f
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-08-05 12:51:00 +0530

    using set -e trap for backend.sh to check automatic exit if error comes kept set and tram in common.sh and using it in mysql.sh

--- backend.sh
+++ backend.sh
@@ -1,60 +1,48 @@
 #!/bin/bash
 
 source ./common.sh
+
 check_root
 
+echo "Please enter DB password:"
+read -s mysql_root_password
+
+
 dnf module disable nodejs -y &>>$LOGFILE
-VALIDATE $? "Disabling default nodejs"
-
 dnf module enable nodejs:20 -y &>>$LOGFILE
-VALIDATE $? "Enabling nodejs:20 version"
-
 dnf install nodejs -y &>>$LOGFILE
-VALIDATE $? "Installing nodejs"
 
 id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     useradd expense &>>$LOGFILE
-    VALIDATE $? "Creating expense user"
 else
     echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>$LOGFILE # -p if file exists silents and don't throw error if file not present creates new file
-VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading backend code"
 
 cd /app
 rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
-VALIDATE $? "Extracted backend code"
 
 npm install &>>$LOGFILE
-VALIDATE $? "Installing nodejs dependencies"
 
 # Giving absolute path
 # Check your path and repo
 cp /home/ec2-user/expense-shell-re/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
-VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Deamon reload"
 
 systemctl start backend &>>$LOGFILE
-VALIDATE $? "Starting backend"
 
 systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Enabling backend"
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL client"
 
 mysql -h db.daws-78s.shop -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
-VALIDATE $? "Restarting backend"
 
